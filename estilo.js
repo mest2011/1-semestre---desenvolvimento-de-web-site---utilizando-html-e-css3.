d6c5578db0365597d1ac9a0b191b09be4d790077
Miguel Eto Sobral Teixeira <mest2011mest@gmail.com>
2019-05-28 13:20:12 -0300

    upload do projeto para o git

---
+++ estilo.js
@@ -0,0 +1,6 @@
+$(function() {
+ $(function() { $('#slider').chocoslider(); });
+</script>
+
+
+Read more: http://www.linhadecodigo.com.br/artigo/3605/banner-rotativo-em-jquery-utilizando-o-choco-slider.aspx#ixzz5G39y5X00
